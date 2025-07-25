f4b755c88849e2f004c3cf30a768709ea6d1a4b3
Gustavo Aguilar <gangeagui@gmail.com>
2025-07-25 11:15:34 -0600

    nuevos cambios

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/index.css";
 import { AuthProvider } from "./context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
