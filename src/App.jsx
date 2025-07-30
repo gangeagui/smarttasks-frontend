90e751f109ae4cf6850f88334ad2b094c1e142f0
Gustavo Aguilar <gangeagui@gmail.com>
2025-07-30 09:52:52 -0600

    nuevas routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ import UsersPage from "./pages/UsersPage";
 import Layout from "./components/Layout";
 import ConfigurationPage from "./pages/ConfigurationPage";
 import RegisterPage from "./pages/RegisterPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      {<Route path="/forgot-password" element={<ForgotPasswordPage />} />}
+      {<Route path="/reset-password" element={<ResetPasswordPage />} />}
       <Route
         element={
           <PrivateRoute>
